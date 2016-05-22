e307f18a73a21c593cb0b9fc01db604fcf6246dd
phrichards <phrichards@gmail.com>
2016-05-22 14:52:07 -0400

    Remove absolute path in auth callback url

--- config/auth.js
+++ config/auth.js
@@ -5,6 +5,6 @@ module.exports = {
 		'clientID' : 'bead3d948b734427b78a867871f38b09',
 		'clientSecret' : '126ec3635c51473a80a9e1dbe4f4655a',
 		// 'callbackURL' : 'http://gumscraper.herokuapp.com/auth/spotify/callback'
-		'callbackURL' : 'http://localhost:8081/auth/spotify/callback'
+		'callbackURL' : '/auth/spotify/callback'
 	}
 }
